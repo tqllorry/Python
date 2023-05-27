f6a7ec5a6cca7ee0af96671246b3b4fb6b83c844
lorrytang <lorrytang@tencent.com>
2023-05-27 12:36:10 +0800

    Changes

--- compressCSV.py
+++ compressCSV.py
@@ -1,29 +1,28 @@
 from pyspark.sql import SparkSession
 
 # 创建SparkSession
-spark = SparkSession.builder.appName("JsonParsingApp").master("local[*]").getOrCreate()
+spark = SparkSession.builder.appName("csvParsingApp").master("local[*]").getOrCreate()
 
 # 读取本地文件
-input_path = "file:///Users/tangqiliang/Documents/files/dept/csv/0215"
+input_path = "file:///Users/TQL/work/data/dept_data/20000101"
 df = spark.read.option("header", True).csv(input_path)
 df.createOrReplaceTempView("my_dept")
 
 # 进行数据清洗和处理
 cleaned_df = spark.sql("""
-    select 
-    company_id,staff_id,dept1,dept2,dept3,dept4,dept5
-     from my_dept
+    select company_id,staff_id,dept1,dept2,dept3,dept4,dept5 from my_dept
+    union all
+    select company_id_copy,staff_id_copy,dept1_copy,dept2_copy,dept3_copy,dept4_copy,dept5_copy from my_dept
 """)
-
+# /Users/TQL/work/data/dept_data/result/20000101
 # cleaned_df.show()
 
 # 将数据写回本地文件
-output_path = "file:///Users/tangqiliang/Documents/files/dept/result/0215"
+output_path = "file:///Users/TQL/work/data/dept_data/result/20000101"
 cleaned_df.repartition(3).write. \
     mode("overwrite"). \
     format("parquet"). \
     save(output_path)
-
 
 # 停止SparkSession
 spark.stop()
