3079802b7838c8349e16b680dd9df983c3cb8669
lorrytang <lorrytang@tencent.com>
2023-07-03 17:57:01 +0800

    Changes

--- test.py
+++ test.py
@@ -1,19 +1,3 @@
-def transform(dict):
-    referrer = dict["referrer"]
-
-    if 'ref' in dict.keys():
-        dict["ref"] = referrer if len(dict["ref"]) == 0 else dict["ref"]
-    else:
-        dict["ref"] = referrer
-
-    return dict
-
-
-aaa = {
-  "referrer": "https://km.lexiangla.net/exams/004d67907e7b11ec8537c68463c07c1b/manage?map=2147&company_from=km",
-  "ref": ""
- }
-
-bbb = transform(aaa)
-
-print(bbb)
+CREATE_SQL = "create table if not exists dashboard_tdw_test.{} like " \
+             "dashboard_tdw_test.ads_disk_nodes_categories_d_f" \
+    .format(TABLE_NAME)
